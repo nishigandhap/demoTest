d5cef2e9b5942c509c2f7e60da95509e8e8b0e7b
Nishigandha <patilnisha3939@gmail.com>
2023-04-10 13:55:29 +0530

    Update intercept code

--- cypress/e2e/APITest/apiTest.js
+++ cypress/e2e/APITest/apiTest.js
@@ -2,15 +2,16 @@
 
 import sampleForm from '../../fixtures/sampleForm.json'
 import updateForm from '../../fixtures/updateTitle.json'
+import multipleForms from '../../fixtures/multipleForms.json'
 
 const apiUrl = `${Cypress.env('Api_url')}`
 const authorization = ` Bearer ${Cypress.env('apiToken')}`
 
 
 describe('APi test suite', () => {
-    beforeEach(() => {
-        cy.cleanUpBeforeStart()
-    })
+    // beforeEach(() => {
+    //     cy.cleanUpBeforeStart()
+    // })
 
     it('Get forms', () => {
         cy.request({
@@ -20,6 +21,7 @@ describe('APi test suite', () => {
         }).should(({ status, body }) => {
             expect(status).to.eq(200)
             expect(body.total_items).is.eq(body.items.length)
+            expect(body)
         })
     })
 
@@ -40,5 +42,30 @@ describe('APi test suite', () => {
                 expect(status).to.eq(200)
                 expect(body.title).is.eq(updateForm.title)
             })
+    })
+
+    it('Method not found', () => {
+        cy.request({
+            method: 'GET',
+            url: `${apiUrl}forms/`,
+            headers: { authorization },
+            failOnStatusCode: false,
+            body: { "code": "NOT_FOUND", "description": "Endpoint not found" }
+        }).should(({ status, body }) => {
+            expect(status).is.eq(404)
+            expect(body.code).to.eq('NOT_FOUND')
+            expect(body.description).to.eq('Endpoint not found')
+        })
+    })
+//Here I am trying to intercept the request and fetch the data from multipleForms.json file.
+    it.only('Intercept the form', () => {
+        cy.intercept({
+            method: 'GET',
+            url: `${apiUrl}forms`,
+            headers: { authorization },
+            body: multipleForms
+        }).should(({ status, body }) => {
+            expect(status).is.eq(200)
+        })
     })
 })
